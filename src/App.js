80dc6e19eeb0fd974e8164d8fa16ab55d398297d
unknown <vanshbaghel912@gmail.com>
2022-11-08 14:58:52 +0530

    Changes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
-import './App.css';
-import Check from './Check';
+import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import NavBar from "./Components/NavBar_MovieTypes/NavBar";
+import MovieDetails from "./Components/Movie_Details/MovieDetails";
+import MoviesList from "./Components/List_Of_Cards/MoviesList";
+import Home from "./Components/Home/Home";
 
 function App() {
   return (
-    <>
-    <Check />
-    </>
+    <div className="App">
+      <Router>
+          <NavBar />
+            <Routes>
+                <Route index path='/Imdb' element={<Home />}></Route>
+                <Route path="movie/:id" element={<MovieDetails />}></Route>
+                <Route path="movies/:type" element={<MoviesList />}></Route>
+                <Route path="/*" element={<h1>Error Page</h1>}></Route>
+            </Routes>
+        </Router>
+    </div>
   );
 }
 
